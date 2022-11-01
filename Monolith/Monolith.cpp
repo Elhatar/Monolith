5cee49ae8129830aa83d93fdf29e7fe4a3957864
01ark <01ark@DESKTOP-7476MUI>
2022-11-01 14:10:53 +0200

    Добавьте файлы проекта.

---
+++ Monolith/Monolith.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    //Put count of your lines in text file here. Right now I'm limited by technical ability of C++ lang. Or my skills ¯\_(ツ)_/¯
+    string line[11];
+
+    srand(time(NULL));
+    setlocale(LC_ALL, "Russian");
+
+    bool monolith_defender_alive = true;
+
+    ifstream file("Monolith.txt");
+    int i = 0;
+
+    if (file.is_open())
+    {
+        while (file.peek()!=EOF)
+        {
+            getline(file, line[i]);
+            i += 1;
+        }
+    }
+    file.close();
+    while (monolith_defender_alive)
+    {
+        int rnd = rand() % i;
+        cin.get();
+        cout << line[rnd];
+    }
+}
